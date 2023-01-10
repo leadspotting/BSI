89770d4b59e28bacad665631795e43606494c3ec
arikBizwatch <104711383+arikBizwatch@users.noreply.github.com>
2023-01-10 22:16:08 +0200

    small update

--- front-end/src/app/shared/models/contact-book-model.ts
+++
@@ -1,4 +0,0 @@
-export interface ContactBookModel {
-  campaigns: any[];
-  defaultList: any;
-}
--- front-end/src/app/shared/services/shared-service.spec.ts
+++
@@ -1,7 +0,0 @@
-import { SharedService } from './shared-service';
-
-describe('SharedService', () => {
-  it('should create an instance', () => {
-    expect(new SharedService()).toBeTruthy();
-  });
-});
