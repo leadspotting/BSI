1cf6d7f867d6cf0e104c54dd7f11d68d88afa883
leadspotting <qais@leadspotting.com>
2024-01-14 22:16:57 +0200

    UI updates

--- front-end/src/app/home/components/carousel/carousel.component.ts
+++ front-end/src/app/home/components/carousel/carousel.component.ts
@@ -18,28 +18,24 @@ export class CarouselComponent implements OnInit {
   screenWidth: any;
   titles = [
     {
-      title1: 'Welcome to your go-to source for high-quality leads lists!',
-      title2: ' Grow your business with high-quality leads from our portal',
-      text2: 'Start boosting your sales today!',
+      title1: 'Welcome to the Israel Technology marketplace introduction space!',
+      title2: 'Search for companies and partners from Israel and from the rest of the world.',
+      text2: 'Request 5 introductions a month.',
     },
     {
-      title1:
-        'Filter with ease and find the perfect leads list for your business',
-      title2:
-        'Find the perfect list for your business by filtering by industry and location',
-      text2:
-        'Custom lists available upon request. Payment is easy and secure with PayPal',
+      title1:'Israel is a hi-tech and international export power.',
+      title2:'its all about the people. Diligent and striving forward.',
+      text2:'We are welcome you to find the right company to do business with.',
     },
     {
-      title1: 'Instant access to targeted leads lists',
-      title2:
-        'Simply find the list you need and make your purchase with LeadSpot',
-      text1: 'no login or registration required',
+      title1: 'The companies presented on the website also specialize in exports.',
+      title2:  'You are welcome to search for the branch that suits your business and get in touch today – its free.',
+      text1: 'This is the right time to do business with Israeli businesses',
     },
     {
-      title1: 'Not sure if our lists are right for you? ',
-      title2: 'Download a free sample of 5 leads to try before you commit',
-      text2: 'You' + "'ll see firsthand the value of our lists from LeadSpot",
+      title1: 'Collaborations between people. between companies.',
+      title2: 'Here you will find a database of companies in different and diverse fields of activity',
+      text2: 'Dive in and find your next Israeli business partners',
     },
   ];
   effect = 'scrollx';
